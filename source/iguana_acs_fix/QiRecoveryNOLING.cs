6475963181381d80d8291a77370cb763caa8b062
ucddj <75850663+ucddj@users.noreply.github.com>
2023-07-27 06:24:40 +0200

    Add desc for translations, changed some number formats, fix the qi recovery number during gc breakthrough

---
+++ source/iguana_acs_fix/QiRecoveryNOLING.cs
@@ -0,0 +1,28 @@
+﻿using XiaWorld;
+using HarmonyLib;
+using System.Collections.Generic;
+using System;
+using System.Reflection;
+
+namespace iguana_acs_fix
+{
+    internal class QiRecoveryNOLING
+    {
+        static void setQiRecoveryToZero(Npc npc, g_emNpcSpecailFlag eFlag, int value, int oldvalue)
+        {
+            npc.DynamicAddLing(0f);
+        }
+        
+        [HarmonyPatch(typeof(ThingFlag<Npc>), "BuildActionDict")]
+        internal class addNOLINGFlag
+        {
+            private static void Postfix(ThingFlag<Npc> __instance)
+            {
+                Dictionary<int, Action<Npc, g_emNpcSpecailFlag, int, int>> dicAction = (Dictionary<int, Action<Npc, g_emNpcSpecailFlag, int, int>>)Traverse.Create(__instance).Field("dicAction").GetValue();
+
+                dicAction[(int)g_emNpcSpecailFlag.FLAG_NOLING] = setQiRecoveryToZero;
+            }
+        }
+    }
+    
+}
